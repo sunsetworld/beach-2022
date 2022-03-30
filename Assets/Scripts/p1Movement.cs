193cf3d5e63f32625674526af849f9b834b781b0
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-30 17:31:11 +0100

    Working on the movement script.

---
+++ Assets/Scripts/p1Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p1Movement : MonoBehaviour
+{
+    Vector2 movement = new Vector2(10, 0);
+    GameObject p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.5f * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.5f * Time.deltaTime, 0, 0);
+        }
+    }
+}
