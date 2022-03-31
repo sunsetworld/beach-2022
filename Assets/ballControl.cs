70f856cbe4a1a36a2d5f4cfeb032b4c9a1ede83d
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-31 07:15:36 +0100

    Making some progress.
    Currently, I'm working on implementing a script for the ball so that it bounces correctly to the other player in the correct direction.

--- Assets/ballControl.cs
+++ Assets/ballControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ballControl : MonoBehaviour
 {
+    CircleCollider2D cc2d;
+    [SerializeField] Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float r1 = Random.Range(1, 2);
+        float r2 = Random.Range(1, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 randomNum = new Vector2(-4, 2);
+        rb2d.AddForce(randomNum);
     }
 }
